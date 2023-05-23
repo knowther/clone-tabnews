bf964e0f06d1012ab2b4c1f2c00b199261a9a7c2
Johnny Wesley <johnnywesley.contato@outlook.com>
2023-05-23 00:13:20 -0300

    adicionada a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return<>
+    <h1>Eduarda, já que lhe devo um cheiro, segura esse ai!</h1>;
+    <img src="https://media.tenor.com/PS6medrGxqwAAAAd/cat-kiss.gif"/>
+  </> 
+  
+}
+
+export default Home;
