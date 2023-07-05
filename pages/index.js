5b320833e531b26f24dac2ad3918259ff60e0919
Johnny Wesley <johnnywesley.contato@outlook.com>
2023-07-05 01:32:11 -0300

    adicionado `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-  return<>
-    <h1>Eduarda, já que lhe devo um cheiro, segura esse ai!</h1>;
-    <img src="https://media.tenor.com/PS6medrGxqwAAAAd/cat-kiss.gif"/>
-  </> 
-  
+  return (
+    <>
+      <h1>Eduarda, já que lhe devo um cheiro, segura esse ai!</h1>;
+      <img src="https://media.tenor.com/PS6medrGxqwAAAAd/cat-kiss.gif" />
+    </>
+  );
 }
 
 export default Home;
